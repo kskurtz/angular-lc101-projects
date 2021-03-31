95b1e3ca622f72950b3c341e788defd49707f35a
Karla <karlakurtz@sbcglobal.net>
2021-03-30 23:20:57 -0500

    changes

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos Assignment';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
-
+  image2 = 'https://images.pexels.com/photos/3857215/pexels-photo-3857215.jpeg?cs=srgb&dl=pexels-nick-wehrli-3857215.jpg&fm=jpg';
+  image3 = 'https://cf-images.us-east-1.prod.boltdns.net/v1/static/5615998036001/8c03b6a8-d2f8-467e-ad16-8d8eab0493fa/0f5104ad-e415-4602-8f9d-63738eb62d07/1280x720/match/image.jpg';
   constructor() { }
 
   ngOnInit() {
